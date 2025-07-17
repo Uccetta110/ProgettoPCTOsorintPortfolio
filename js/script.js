dc92a203541df0258404bf9d1b6aa44935037bbd
Uccetta110 <herobrinesonoio2.0@gmail.com>
2025-07-17 09:41:06 +0200

    secondo caricamento

--- js/script.js
+++ js/script.js
@@ -5,11 +5,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   aboutButton.addEventListener("click", () => {
     console.log("About button clicked");
+    window.scrollBy({
+      top: 200,
+      behavior: "smooth"
+    });
   });
   schoolButton.addEventListener("click", () => {
     console.log("School button clicked");
+        window.scrollBy({
+      top: 400,
+      behavior: "smooth"
+    });
   });
   projectsButton.addEventListener("click", () => {
     console.log("Projects button clicked");
+        window.scrollBy({
+      top: 600,
+      behavior: "smooth"
+    });
+  });
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+
+    }
   });
 });
