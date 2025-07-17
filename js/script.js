68283a4c3ac1edfb57f2dd2dca80227a826d73e3
Uccetta110 <herobrinesonoio2.0@gmail.com>
2025-07-17 14:31:24 +0200

    responsive

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let schoolImg = document.getElementById("school-img");
   let indirizzoImg = document.getElementById("indirizzo-img");
   let mapsImg = document.getElementById("maps-img");
+  let body = document.getElementById("body");
 
   aboutButton.addEventListener("click", () => {
     console.log("About button clicked");
@@ -43,16 +44,27 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("School image clicked");
     window.open("https://www.itispaleocapa.edu.it/istituto/informatica/");
   });
-    mapsImg.addEventListener("click", () => {
+  mapsImg.addEventListener("click", () => {
     console.log("School image clicked");
     window.open("https://www.itispaleocapa.edu.it/istituto/dove-siamo/");
   });
 
   document.addEventListener("scroll", () => {
+    console.log(window.scrollY);
     if (window.scrollY > 10) {
       homeButton.setAttribute("style", "visibility: visible;");
     } else {
       homeButton.setAttribute("style", "visibility: hidden;");
+    }
+
+    if (window.scrollY < 600) {
+      body.setAttribute("style", "background-color: #eef4ed");
+    }
+    if (window.scrollY >= 600 && window.scrollY < 1400) {
+      body.setAttribute("style", "background-color: #8DA9C4");
+    }
+    if (window.scrollY >= 1400) {
+      body.setAttribute("style", "background-color: #134074");
     }
   });
 });
